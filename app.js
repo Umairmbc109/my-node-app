224b8b887afb92c20e6c86fadcd550b8c1d7dd32
Umair <umair.khan@gmail.com>
2024-05-18 03:11:12 +0500

    Dockrization of node-app

---
+++ app.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+const port = 3000;
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    return;
+  }
+  console.log('Connected to the database');
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`App running at http://localhost:${port}`);
+});
